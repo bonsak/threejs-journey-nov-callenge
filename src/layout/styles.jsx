35b4e52cc845a66360bd349943bc7f6e4d5d9363
bonsak <bonsak@racecar.no>
2023-10-29 14:15:23 +0100

    Add font

--- src/layout/styles.jsx
+++ src/layout/styles.jsx
@@ -9,4 +9,15 @@ export const TopLeft = styled.div`
   position: absolute;
   top: 5vw;
   left: 5vw;
+  & h1 {
+    font-size: 6rem;
+    color: #db8400;
+    padding: 0;
+    margin: 0 0 0.05em 0;
+    font-family: 'talon', sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    /* -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: #ffffff; */
+  }
 `
