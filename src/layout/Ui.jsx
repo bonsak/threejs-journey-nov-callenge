7e5c07fd4a9826d4fe3da58a6fc0293406e3c600
bonsak <bonsak@racecar.no>
2023-10-30 17:08:08 +0100

    Fix selector

--- src/layout/Ui.jsx
+++ src/layout/Ui.jsx
@@ -5,6 +5,19 @@ export default function Ui() {
   const { soundIsOn, toggleSound } = useSoundStore()
   const { grumpkinType, setGrumpkinType } = useGrumpkinStore()
   const grumpkinNames = ['Harold', 'Theodore', 'Ebenezer', 'Maximillian']
+  const grumpkins = [
+    { gName: 'Harold', active: false },
+    { gName: 'Theodore', active: false },
+    { gName: 'Ebenezer', active: false },
+    { gName: 'Maximillian', active: false },
+  ]
+
+  grumpkins.map((grumpkin, index) => {
+    if (parseInt(grumpkinType) === index + 1) {
+      grumpkin.active = true
+    }
+    console.log(index + 1, grumpkin.gName, grumpkin.active)
+  })
 
   const handleSoundToogle = () => {
     toggleSound()
@@ -25,24 +38,29 @@ export default function Ui() {
       </div>
       <div className='bottom-wrapper'>
         <div className='selector'>
-          {grumpkinNames.map((grumpName, index) => (
-            <button
-              value={index + 1}
-              key={grumpName}
-              className='button grumpkinBtn'
-              onClick={(e) => {
-                setGrumpkinType(e.currentTarget.value)
-              }}
-            >
-              {grumpName}
-            </button>
-          ))}
+          {grumpkins.map((grumpkin, index) => {
+            return (
+              <button
+                value={index + 1}
+                key={grumpkin.gName}
+                onClick={(e) => setGrumpkinType(e.currentTarget.value)}
+                className={
+                  grumpkin.active
+                    ? 'button grumpkinBtn active'
+                    : 'button grumpkinBtn'
+                }
+              >
+                {grumpkin.gName}
+              </button>
+            )
+          })}
+          {grumpkinType}
         </div>
         <button
           onClick={handleSoundToogle}
-          className={!soundIsOn ? 'button sound' : 'button sound sound-off'}
+          className={!soundIsOn ? 'button sound sound-off' : 'button sound'}
         >
-          {!soundIsOn ? 'SOUND OFF' : 'SOUND ON'}
+          {!soundIsOn ? 'SOUND ON' : 'SOUND OFF'}
         </button>
         <div className='bottom-text'>Made by Bonsak 2023</div>
       </div>
