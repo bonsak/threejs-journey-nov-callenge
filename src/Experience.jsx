95d7e0a545722833cad27825471c5147cc5b3c5a
bonsak <bonsak@racecar.no>
2023-10-31 15:45:47 +0100

    Add favicon

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -28,7 +28,7 @@ export default function Experience() {
         position={[0, 0, 0]}
         swearingStartPoint={0}
       />
-      <CameraControls
+      {/* <CameraControls
         ref={cameraRef}
         minDistance={3}
         maxDistance={12}
@@ -38,15 +38,15 @@ export default function Experience() {
         // enabled={enabled}
         // verticalDragToForward={verticalDragToForward}
         // infinityDolly={infinityDolly}
-      />
-      {/* <OrbitControls
+      /> */}
+      <OrbitControls
         makeDefault
         minDistance={3}
         maxDistance={12}
         maxPolarAngle={Math.PI / 3 + 0.6}
         enablePan={false}
         target={[0, 1.25, 0]}
-      /> */}
+      />
       <Environment
         files='./textures/env-v01.hdr'
         ground={{
